1d7fc2169edddad59cc22a0b6c15716cb62d44f9
Spruce Bondera <sprucebondera@gmail.com>
2017-11-15 00:18:33 +0000

    Add missing interpolated_learning file

---
+++ double-dqn-baseline/interpolated_learning_rate.py
@@ -0,0 +1,28 @@
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.framework import dtypes
+
+def interpolated_decay(start_lr, end_lr, global_step, start_step, end_step,
+                       name=None):
+    """ TODO explain"""
+    if global_step is None:
+        raise ValueError("global_step is required for interpolated_decay.")
+    with ops.name_scope(name, "PolynomialDecay", [start_lr, end_lr, global_step,
+                                                  start_step, end_step]) as name:
+        start_lr = ops.convert_to_tensor(start_lr, name="start_lr")
+        dtype = start_lr.dtype
+        end_lr = math_ops.cast(end_lr, dtype)
+        global_step = math_ops.cast(global_step, dtype)
+        start_step = math_ops.cast(start_step, dtype)
+        end_step = math_ops.cast(end_step, dtype)
+
+        # Make sure that the global_step used is not bigger than decay_steps.
+        global_step = math_ops.minimum(global_step, end_step)
+        global_step = math_ops.maximum(global_step, start_step)
+        global_step = math_ops.subtract(global_step, start_step)
+
+        decay_steps = math_ops.subtract(end_step, start_step)
+        p = math_ops.div(global_step, decay_steps)
+
+        return math_ops.add(math_ops.multiply(start_lr - end_lr, 1 - p),
+                            end_lr, name=name)
