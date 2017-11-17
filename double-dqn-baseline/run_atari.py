f7a158ddb085d19da61e18838aa0eeff662359d6
Spruce Bondera <sprucebondera@gmail.com>
2017-11-17 15:44:13 -0500

    Add more logging to baseline

--- double-dqn-baseline/run_atari.py
+++ double-dqn-baseline/run_atari.py
@@ -22,8 +22,10 @@ def main():
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--prioritized', type=int, default=1)
     parser.add_argument('--num-timesteps', type=int, default=int(10e6))
+    parser.add_argument('experiment_id')
     args = parser.parse_args()
-    logger.configure()
+    logger.configure('./{}--{}'.format(args.experiment_id, args.env),
+                     ['stdout', 'tensorboard', 'json'])
     set_global_seeds(args.seed)
     env_name = args.env + "NoFrameskip-v4"
     env = make_atari(env_name)
