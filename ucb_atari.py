68195f7504e0df61cc4d593f500ae3c4cc2baedc
jhmitchel <jhmitchel@gmail.com>
2017-11-19 20:38:04 -0500

    changed the multiheaded code to have right hyperparamters, use experiments dir, etc.

--- ucb_atari.py
+++ ucb_atari.py
@@ -15,15 +15,23 @@ from baselines import bench
 import argparse
 from baselines import logger
 from baselines.common.atari_wrappers import make_atari
+from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', default='Breakout')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--prioritized', type=int, default=1)
+    parser.add_argument('--prioritized', type=int, default=0)
     parser.add_argument('--num-timesteps', type=int, default=int(10e6))
+    parser.add_argument('experiment_id')
     args = parser.parse_args()
-    logger.configure(dir='./tensorlogs', format_strs=['tensorboard'])
+    logging_directory = Path('./experiments/{}--{}'.format(args.experiment_id, args.env))
+    if not logging_directory.exists():
+        logging_directory.mkdir(parents=True)
+    logger.configure(str(logging_directory), ['stdout', 'tensorboard', 'json'])
+    model_directory = logging_directory / 'models'
+    if not model_directory.exists():
+        model_directory.mkdir(parents=True)
     set_global_seeds(args.seed)
     env_name = args.env + "NoFrameskip-v4"
     env = make_atari(env_name)
@@ -31,26 +39,30 @@ def main():
     env = deepq.wrap_atari_dqn(env)
     model = multiheaded_model.multiheaded
     exploration_schedule = PiecewiseSchedule(
-        endpoints=[(0, 1), (10e6, 0.1), (5 * 10e6, 0.01)], outside_value=0.01)
+        endpoints=[(0, 1), (1e6, 0.1), (5 * 1e6, 0.01)], outside_value=0.01)
     act = learn(
         env,
         q_func=model,
-        lr=1e-4,
         beta1=0.9,
         beta2=0.99,
         epsilon=1e-4,
         max_timesteps=args.num_timesteps,
         buffer_size=1000000,
         exploration_schedule=exploration_schedule,
+        start_lr=1e-4,
+        end_lr=5 * 1e-5,
+        start_step=1e6,
+        end_step=5 * 1e6,
         train_freq=4,
-        print_freq=1,
+        print_freq=10,
         batch_size=32,
         learning_starts=50000,
         target_network_update_freq=10000,
         gamma=0.99,
         prioritized_replay=bool(args.prioritized),
+        model_directory=model_directory
     )
-    # act.save("pong_model.pkl") XXX
+    act.save(str(model_directory / "act_model.pkl"))
     env.close()
 
 
